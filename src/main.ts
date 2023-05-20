ac772aa671d089bd4e51c9d7b89616064fc555cb
woow-wu7 <woow.wu7@gmail.com>
2023-05-20 23:29:20 +0800

    feat: use divine-plus.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import piniaResetPlugin from '@/plugins/pinia/reset'
 
+import DivinePlus from "divine-plus";
+import "divine-plus/css/index.css";
+
 import App from './App.vue'
 import router from './router'
 
@@ -18,5 +21,5 @@ pinia.use(piniaResetPlugin) // pinia reset plugin
 
 app.use(pinia)
 app.use(router)
-
+app.use(DivinePlus);
 app.mount('#app')
