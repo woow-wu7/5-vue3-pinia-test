3c1ead8d98fa3d1985732c56ae5ffc95bede1df1
woow-wu7 <woow.wu7@gmail.com>
2023-05-21 13:09:17 +0800

    feat: install divine-plus1.1.7 suppert ts.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import piniaResetPlugin from '@/plugins/pinia/reset'
 
-import DivinePlus from "divine-plus";
-import "divine-plus/css/index.css";
+import DivinePlus from 'divine-plus'
+import 'divine-plus/css/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -21,5 +21,5 @@ pinia.use(piniaResetPlugin) // pinia reset plugin
 
 app.use(pinia)
 app.use(router)
-app.use(DivinePlus);
+app.use(DivinePlus)
 app.mount('#app')
