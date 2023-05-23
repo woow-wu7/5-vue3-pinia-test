4167642ac3db3662e928fc6a26d243d83342a601
woow-wu7 <woow.wu7@gmail.com>
2023-05-23 19:25:17 +0800

    feat: props.

--- src/components/PropsChild.vue
+++ src/components/PropsChild.vue
@@ -25,9 +25,12 @@ const aa = computed(() => {
 
 onUpdated(() => {
   console.log('子组件更新了')
-  console.log('1. 当props是父组件的响应式对象，子组件有使用props时，父组件更新，子组件就会更新')
+
   console.log(
-    '2. 当子组件内没有使用任何props，即使props是父组件中的响应式对象，当props变化，子组件也不会重新渲染'
+    '1. 当props是父组件的响应式对象，子组件有使用props时，父组件更新props中的属性，子组件就会更新'
+  )
+  console.log(
+    '2. 当子组件内没有使用任何props，即使props是父组件中的响应式对象，当父组件响应式对象变化，即子组件props变化，子组件也不会重新渲染'
   )
 })
 
